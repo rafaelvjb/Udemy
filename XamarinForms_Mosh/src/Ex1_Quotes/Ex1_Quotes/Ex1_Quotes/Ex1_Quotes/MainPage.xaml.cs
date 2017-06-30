d73c097e2117f8cd513ec57d6e493ded7ab28469
rafael brunello <rafael.brunello@kroton.com.br>
2017-06-30 11:14:22 -0300

    incluindo os repositorios utilizados no note para estudo

---
+++ XamarinForms_Mosh/src/Ex1_Quotes/Ex1_Quotes/Ex1_Quotes/Ex1_Quotes/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Ex1_Quotes
+{
+    public partial class MainPage : ContentPage
+    {
+        private readonly List<string> _frases = new List<string>();
+        private int _indexFrase = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            for (var i = 0; i < 5; i++)
+            {
+                _frases.Add($"Frase escrita concatenada: {i}");
+            }
+
+            Frases.Text = _frases.ElementAt(0);
+        }
+
+        private void ProximaFrase_OnClicked(object sender, EventArgs e)
+        {
+            _indexFrase = _indexFrase == _frases.Count - 1 ? 0 : _indexFrase + 1;
+            
+            Frases.Text = _frases.ElementAt(_indexFrase);
+        }
+    }
+}
